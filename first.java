a3574f5bd51e54ab605f047cad825ccf375c8b19
edurekadilip <draheja@msn.com>
2020-08-25 20:53:33 +0530

    add before commit after changing

--- first.java
+++ first.java
@@ -3,6 +3,8 @@ class dilip {
   system.out.println("This is first line");
   // Adding after commit
   system.out.println("This is second line");
+  // Adding after commit check if add needed after editing
+  system.out.println("This is third line");
   }
 }
 
